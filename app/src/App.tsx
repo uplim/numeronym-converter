5f1220ea1618ff06531618f1aa8da72074b38743
Daiki Maekawa <daaikii@outlook.jp>
2022-09-20 23:29:37 +0900

    emotion導入

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { Global } from '@emotion/react'
+import { globalStyle } from '@/styles/global-style'
 
 const App = () => {
-  return <>{'aaaaaa'}</>
+  return (
+    <>
+      <Global styles={globalStyle} />
+      <div>あああ</div>
+    </>
+  )
 }
 
 export default App
---
+++ app/src/styles/global-style.ts
@@ -0,0 +1,8 @@
+import { css } from '@emotion/react'
+
+export const globalStyle = css`
+  // 適宜globalに必要そうなCSSを書いていく
+  body {
+    font-family: 'Courier New', Courier, monospace;
+  }
+`
