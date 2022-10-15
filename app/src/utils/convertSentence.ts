c94922f852251b60cb22dc685c01089645451774
ibuki <64850924+eve00@users.noreply.github.com>
2022-10-15 19:45:48 +0900

    余計なコードを修正

--- app/src/utils/convertSentence.ts
+++ app/src/utils/convertSentence.ts
@@ -4,10 +4,9 @@ import { useIO } from '@/hooks/useIO'
 
 export const convertSentence = () => {
   // 入力と出力の定義
-  const [sentence, setSentence] = useState('')
-  const { setOutput } = useIO()
+  const { input, setOutput } = useIO()
 
-  const arr = splitSentence(sentence)
+  const arr = splitSentence(input)
 
   // arrをごにょごにょして最終これを返す
   setOutput('are you a b6l fun?')
