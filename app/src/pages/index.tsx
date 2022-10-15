1f2c13a286f324477cbdd796e86a7a903e5ac27f
ibuki <64850924+eve00@users.noreply.github.com>
2022-10-15 19:30:23 +0900

    入出力処理の変更part2

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { TextBox } from '@/components/TextBox'
 import { useIO } from '@/hooks/useIO'
+import { Button } from '@/components/Buttons'
+import { convertSentence } from '@/utils/convertSentence'
 
 export const Home = () => {
   const { input, output, onChange } = useIO()
@@ -10,7 +12,8 @@ export const Home = () => {
       <Link to={`/about/`}>/about</Link>
       <div>
         <h1>ぬめろにむこんばーた</h1>
-        <TextBox canInput={true} />
+        <TextBox canInput={true} onChange={(e) => onChange(e)}/>
+        <Button label={'へんかん'} onClick={convertSentence} />
         <TextBox canInput={false} />
       </div>
     </>
