0596ebecbe13d25f8e0c75fc4baa2c52d262e693
whooolemilk <weekender.nyanko@gmail.com>
2022-10-15 19:16:05 +0900

    storybookにSwitchButtonコンポーネントを登録

---
+++ app/src/stories/components/Buttons/SwitchButton.stories.tsx
@@ -0,0 +1,10 @@
+import { StoryObj, ComponentMeta } from '@storybook/react'
+import { SwitchButton } from '@/components/Buttons'
+export default {
+  component: SwitchButton,
+  argTypes: {} // 今後追加される予定
+} as ComponentMeta<typeof SwitchButton>
+
+export const Default: StoryObj = {
+  args: {} // 今後追加される予定
+}
