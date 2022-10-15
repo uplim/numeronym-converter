a5573dd98454f7e0a52aba467a317cb9557092aa
whooolemilk <weekender.nyanko@gmail.com>
2022-10-15 19:23:43 +0900

    ci落ちたので修正

--- app/src/stories/components/Buttons/SwitchButton.stories.tsx
+++ app/src/stories/components/Buttons/SwitchButton.stories.tsx
@@ -1,10 +1,7 @@
 import { StoryObj, ComponentMeta } from '@storybook/react'
 import { SwitchButton } from '@/components/Buttons'
 export default {
-  component: SwitchButton,
-  argTypes: {} // 今後追加される予定
+  component: SwitchButton
 } as ComponentMeta<typeof SwitchButton>
 
-export const Default: StoryObj = {
-  args: {} // 今後追加される予定
-}
+export const Default: StoryObj = {}
