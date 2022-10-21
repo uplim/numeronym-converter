fe17e19f1d68975a3123fab56667b4b1c20d977b
ibuki <64850924+eve00@users.noreply.github.com>
2022-10-22 00:22:54 +0900

    コンバータ機能を実装（仮）

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -6,15 +6,15 @@ import { Button } from '@/components/Buttons'
 import { convertSentence } from '@/utils/convertSentence'
 
 export const Home = () => {
-  const { input, output, onChange } = useIO()
+  const {setOutput, onChange} = useIO()
   return (
     <>
       <Link to={`/about/`}>/about</Link>
       <div>
         <h1>ぬめろにむこんばーた</h1>
         <TextBox canInput={true} onChange={(e) => onChange(e)} />
-        <Button label={'へんかん'} onClick={convertSentence} />
-        <TextBox canInput={false} />
+        <Button label={'へんかん'} onClick={() => setOutput(convertSentence)} />    
+        <TextBox canInput={false} value={convertSentence()} />
       </div>
     </>
   )
